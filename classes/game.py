ebaa2a02ae5ab6dcb26494b5ed6f1d16623cb7b0
hyduxalt <85044595+hyduxalt@users.noreply.github.com>
2024-08-15 20:46:34 +0800

    Create game.py

---
+++ classes/game.py
@@ -0,0 +1,57 @@
+import random
+import pprint
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class Person:
+    def __init__(self, name, hp, mp, atk, df, magic, items):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.items = items
+        self.actions = ["Attack", "Magic", "Items"]
+        self.name = name
+
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    def heal(self, dmg):
+        self.hp += dmg
+        if self.hp > self.maxhp:
+            self.hp = self.maxhp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
