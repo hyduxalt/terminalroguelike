c3ab5340c54dd574fc9fd57612884ba376e9cf0b
hyduxalt <85044595+hyduxalt@users.noreply.github.com>
2024-08-16 16:11:09 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+from classes.game import Person, bcolors
+from classes.magic import Spell
+from classes.inventory import Item
+import random
+
+
+# Create Black Magic
+fire = Spell("Fire", 25, 600, "black")
+thunder = Spell("Thunder", 25, 600, "black")
+blizzard = Spell("Blizzard", 25, 600, "black")
+meteor = Spell("Meteor", 40, 1200, "black")
+quake = Spell("Quake", 14, 140, "black")
+
+# Create White Magic
+cure = Spell("Cure", 25, 620, "white")
+cura = Spell("Cura", 32, 1500, "white")
+curaga = Spell("Curaga", 50, 6000, "white")
+
+
+# Create some Items
+potion = Item("Potion", "potion", "Heals 50 HP", 50)
+hipotion = Item("Hi-Potion", "potion", "Heals 100 HP", 100)
+superpotion = Item("Super Potion", "potion", "Heals 1000 HP", 1000)
+elixer = Item("Elixer", "elixer", "Fully restores HP/MP of one party member", 9999)
+hielixer = Item("MegaElixer", "elixer", "Fully restores party's HP/MP", 9999)
+
+grenade = Item("Grenade", "attack", "Deals 500 damage", 500)
+
+
+player_spells = [fire, thunder, blizzard, meteor, cure, cura]
+enemy_spells = [fire, meteor, curaga]
+player_items = [{"item": potion, "quantity": 15}, {"item": hipotion, "quantity": 5},
+                {"item": superpotion, "quantity": 5}, {"item": elixer, "quantity": 5},
+                {"item": hielixer, "quantity": 2}, {"item": grenade, "quantity": 5}]
+
+
+# Instantiate People
+player1 = Person("Valos:", 3260, 132, 300, 34, player_spells, player_items)
+player2 = Person("Nick :", 4160, 188, 311, 34, player_spells, player_items)
+player3 = Person("Robot:", 3089, 174, 288, 34, player_spells, player_items)
+
+enemy1 = Person("Imp  ", 1250, 130, 560, 325, enemy_spells, [])
+enemy2 = Person("Magus", 18200, 701, 525, 25, enemy_spells, [])
+enemy3 = Person("Imp  ", 1250, 130, 560, 325, enemy_spells, [])
+
+
+players = [player1, player2, player3]
+enemies = [enemy1, enemy2, enemy3]
+
+running = True
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
+
+while running:
+    print("======================")
+
+    print("\n\n")
+    print("NAME                 HP                                     MP")
+    for player in players:
+        player.get_stats()
+
+    print("\n")
+
+    for enemy in enemies:
+        enemy.get_enemy_stats()
